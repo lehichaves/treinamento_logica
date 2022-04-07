d5683e7c00b1fd1ab4e4170972a5d60687190361
Lehi Chaves <lehi_chaves_93@hotmail.com>
2022-04-07 11:31:43 -0300

    Exercício 5 concluído

--- index.js
+++ index.js
@@ -26,3 +26,19 @@ function multiplica (a, b){
 console.log(multiplica(2,4));
 
 //Exercício 5
+
+function podeDirigir (idade) {
+    if (idade >= 18) {
+        return 'Você pode dirigir'
+    } else {
+        return 'Você não pode dirigir'
+    }
+}
+console.log(podeDirigir(28))
+
+//
+
+
+
+
+
