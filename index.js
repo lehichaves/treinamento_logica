d15efd6a382f9474c60efd0d756fdfc551d980d6
Lehi Chaves <lehi_chaves_93@hotmail.com>
2022-04-06 21:19:40 -0300

    Exercícos aula inicial de javascript

---
+++ index.js
@@ -0,0 +1,47 @@
+// Exercício 1
+
+var nomeCompleto = "Lehi Willian Costa Chaves";
+    console.log(nomeCompleto);
+
+// Exercício 2
+
+var numeroA = 10;
+var numeroB = 7;
+
+var resultado = numeroA * numeroB;
+    console.log(resultado);
+
+// Exercício 3
+
+
+
+
+
+
+// Exercício 5
+let idade = 28;
+
+function podeDirigir (idade) {
+    if (idade < 18) {    
+        return "Você pode dirigir!" }
+    }  
+    else {
+        return "Você não pode dirigir"
+    }
+    console.log(podeDirigir(idade));
+}
+
+
+
+//Exercício 6
+
+for (i=0 ; i <= 20 ; i ++) {
+    if (i % 2 = 1) {
+        console.log (i)
+    }
+}
+      
+// Exercício 7
+
+let numeroTabuada = 1;
+function tabuada (numeroTabuada)
